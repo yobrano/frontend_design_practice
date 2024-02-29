fdf51af63493a8451bea011814f60f5bbccb0f86
yobrano <mburubrian2@gmail.com>
2024-02-29 20:07:12 +0300

    Dynamic task creation

--- HTML-Grid/index.js
+++ HTML-Grid/index.js
@@ -1,216 +1,319 @@
-
-const weekDays = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-];
-const dayHours = [
-    "1:00AM",
-    "2:00AM",
-    "3:00AM",
-    "4:00AM",
-    "5:00AM",
-    "6:00AM",
-    "7:00AM",
-    "8:00AM",
-    "9:00AM",
-    "10:00AM",
-    "11:00AM",
-    "12:00AM",
-    "1:00PM",
-    "2:00PM",
-    "3:00PM",
-    "4:00PM",
-    "5:00PM",
-    "6:00PM",
-    "7:00PM",
-    "8:00PM",
-    "9:00PM",
-    "10:00PM",
-    "11:00PM",
-    "12:00PM",
-];
-
-const timeTable = weekDays.map((day) => {
-    return dayHours.map((hour) => {
-        return {
-            day,
-            hour,
-            completed: false,
-            date: "",
-            startTime: "", // 24 hrs
-            endTime: "",// 24 hrs
-            duration: "", // in hrs
-            title: "",
-            description: "",
-        };
-    });
-});
+const tasks = []
+let taskFormIsOpen = false;
 
 
 // ------------ Helper Functions ------------
-function timeDiff(t2, t1){
-    // t2 --> end 
-    // t1 --> Start
+function timeDiff(t2, t1) {
+    // t2 --> end time, 
+    // t1 --> Start time
+    // Return --> t2 - t1 (in hours)
 
     const time1 = new Date()
     const [t1Hours, t1Minutes] = t1.split(":")
     time1.setHours(t1Hours)
     time1.setMinutes(t1Minutes)
-    
+
     const time2 = new Date()
     const [t2Hours, t2Minutes] = t2.split(":")
     time2.setHours(t2Hours)
     time2.setMinutes(t2Minutes)
 
-    minDiff = ((time2 - time1)/1000)/60
-    hrsDiff = minDiff/60
+    minDiff = ((time2 - time1) / 1000) / 60
+    hrsDiff = minDiff / 60
     console.log(hrsDiff)
     return hrsDiff
 
 }
-const getTask = (day, hour) => {
-    const dayIndex = weekDays.indexOf(day);
-    const hourIndex = dayHours.indexOf(hour)
-    console.log(dayIndex, hourIndex)
-    return timeTable[dayIndex][hourIndex]
+
+function to24Hrs(time){
+    let isPM = time.slice(-2) 
+    let [hours, minutes] = time.slice(0, -2).split(":")
+    if(isPM){
+        hours = Number(hours) + 12
+    }else if(hours === "12"){
+        hours = "00"
+    }
+    return `${hours}:${minutes}`
 }
 
-const setTask = (day, hour, task) => {
-    const dayIndex = weekDays.indexOf(day);
-    const hourIndex = dayHours.indexOf(hour)
-    timeTable[dayIndex][hourIndex] = task
-    return true
+
+function getTaskByHour({day, hour}){
+    
+    const task = tasks.filter(task=>{
+        return task.hour === hour && task.day === day 
+    })[0]
+    return task
 }
+
+function getTaskByID(taskID) {
+    console.log(taskID)
+    return tasks.filter(task => task.id === taskID)[0]
+}
+
+function updateOrCreateTask(taskID) {
+    // Read fomr data.
+    const id = taskForm.querySelector("input[name=task_id]").value
+    const day = taskForm.querySelector("input[name=task_day]").value
+    let hour = taskForm.querySelector("input[name=task_hour]").value
+    const date = taskForm.querySelector("input[name=task_date]").value
+    const title = taskForm.querySelector("input[name=task_title]").value
+    const endTime = taskForm.querySelector("input[name=task_end_time]").value
+    const completed = taskForm.querySelector("input[name=task_completed]").checked
+    const startTime = taskForm.querySelector("input[name=task_start_time]").value
+    const description = taskForm.querySelector("textarea[name=task_description]").value
+    
+    // Calculated Fields.
+    const duration = (endTime!=="" && startTime!=="")? timeDiff(endTime, startTime) : ""
+    if (startTime) {
+        let startHour = Number(startTime.split(":")[0])
+        hour = (startHour < 12) ? `${startHour}:00AM` : `${(startHour - 12)}:00PM`
+        hour = (startHour === 0) ? "12:00AM": hour
+    }
+    
+    const taskItem = {id, day, hour, completed, date, startTime, endTime, duration, title, description }
+
+    // Create if id is null or ""
+    if(id){
+        // update Task
+        const taskIndex = tasks.indexOf(getTaskByID(taskID))
+        tasks[taskIndex] = taskItem
+    }else{
+        // Create Task
+        taskItem.id = crypto.randomUUID()
+        tasks.push(taskItem)
+    }
+    return taskItem
+}
+
+function deleteTask(taskID){
+    const taskIndex = tasks.findIndex(task=> task.id === taskID)
+    tasks.splice(taskIndex, 1)
+}
+
 
 // ------------ Elements Selection ------------
 const taskForm = document.querySelector("#task-form");
 const hours = document.querySelectorAll("#Hour");
-const tasks = document.querySelectorAll(".task");
-
+const taskElements = document.querySelectorAll(".task");
+const deleteBtn = document.querySelector("#delete")
 
 // ------------ Form Control Methods ------------
 function hideTaskForm() {
+
+    taskForm.querySelector("input[name=task_id]").value = "";
     taskForm.querySelector("input[name=task_hour]").value = "";
     taskForm.querySelector("input[name=task_day]").value = "";
-    taskForm.querySelector("input[name=task_completed]").value = false;
+    taskForm.querySelector("input[name=task_completed]").checked = false;
     taskForm.querySelector("input[name=task_date]").value = "";
     taskForm.querySelector("input[name=task_start_time]").value = "";
     taskForm.querySelector("input[name=task_end_time]").value = "";
     taskForm.querySelector("input[name=task_title]").value = "";
     taskForm.querySelector("textarea[name=task_description]").value = "";
-
     taskForm.style["display"] = "none";
+    document.querySelector(".selected-hour")?.classList.remove("selected-hour");
+
+    taskFormIsOpen = false;
 }
 
-function showTaskForm(task, pos) {
-    
-    task = getTask(task.day, task.hour)
+function showTaskForm(clickedTask, pos) {
 
-    taskForm.querySelector("input[name=task_hour]").value = task["hour"];
-    taskForm.querySelector("input[name=task_day]").value = task["day"];
-    taskForm.querySelector("input[name=task_completed]").value =task["completed"];
-    taskForm.querySelector("input[name=task_date]").value = task["date"];
-    taskForm.querySelector("input[name=task_start_time]").value =task["startTime"];
-    taskForm.querySelector("input[name=task_end_time]").value = task["endTime"];
-    taskForm.querySelector("input[name=task_title]").value = task["title"];
-    taskForm.querySelector("textarea[name=task_description]").value =task["description"];
+    const task = getTaskByHour(clickedTask)
+    if(!task){
+        taskForm.querySelector("input[name=task_day]").value = clickedTask["day"] ;
+        taskForm.querySelector("input[name=task_hour]").value = clickedTask["hour"] ;
+    }else{
+        taskForm.querySelector("input[name=task_id]").value = task["id"] ;
+        taskForm.querySelector("input[name=task_date]").value = task["date"] ;
+        taskForm.querySelector("input[name=task_title]").value = task["title"];
+        taskForm.querySelector("input[name=task_day]").value = clickedTask["day"] ;
+        taskForm.querySelector("input[name=task_hour]").value = clickedTask["hour"];
+        taskForm.querySelector("input[name=task_end_time]").value = task["endTime"];
+        taskForm.querySelector("input[name=task_completed]").value = task["completed"];
+        taskForm.querySelector("input[name=task_start_time]").value = task["startTime"];
+        taskForm.querySelector("textarea[name=task_description]").value = task["description"];
+    }
 
-    taskForm.style["position"] = "absolute";
-    taskForm.style["display"] = "block";
 
     const y = pos.y + window.scrollY;
+    taskForm.style["position"] = "absolute";
     taskForm.style["left"] = pos.x + "px";
     taskForm.style["top"] = y + "px";
+    taskForm.style["display"] = "block";
+    
+    taskFormIsOpen = true;
 }
 
-
-function redrawTask(task){
-    const height = Math.max(task["duration"] * 3, 1)
-    task = document.querySelector(".task") 
-    task.style["height"] = height + "rem"
-
-
-}
 
 // ------------ Event Handlers ------------
-function handleTaskSubmit(event) {
-    event.preventDefault();
-    const submittedDay = event.target.querySelector("input[name=task_day]").value;
-    const submittedHour = event.target.querySelector(
-        "input[name=task_hour]"
-    ).value;
+// -------- Keydown handlers
+function handleTaskFormClose(event) {
+    if (event.key === "Escape" && taskFormIsOpen) {
+        hideTaskForm()
+}
+}
 
+// ----------- Click Handlers
+function handleTaskLeftClick(element) {
+    hideTaskForm()
+    element.parentElement.classList.add("selected-hour");
     
-    const oldTask = getTask(submittedDay, submittedHour)
+    const clickedTask = element.parentElement.dataset;
+    const boundingRect = element.parentElement.getBoundingClientRect();
+    showTaskForm(clickedTask, boundingRect);
+}
 
-    const newTask = {
-        ...oldTask,
-        completed: event.target.querySelector("input[name=task_completed]").value,
-        date: event.target.querySelector("input[name=task_date]").value,
-        startTime: event.target.querySelector("input[name=task_start_time]").value,
-        endTime: event.target.querySelector("input[name=task_end_time]").value,
-        title: event.target.querySelector("input[name=task_title]").value,
-        description: event.target.querySelector("textarea[name=task_description]")
-            .value,
-    };
+function handleTaskSubmit(event){
 
-    newTask["duration"] = timeDiff(newTask["endTime"], newTask["startTime"])
-    console.log(newTask)
-    setTask(submittedDay, submittedHour, newTask)
-
-    hideTaskForm();
-    redrawTask(newTask)
-    document.querySelector(".selected-hour").classList.remove("selected-hour");
+    event.preventDefault()
+    const id = taskForm.querySelector("input[name=task_id]").value
+    console.log(id)
+    const task = getTaskByID(id)
+    //create if task is empty 
+    task? updateOrCreateTask(task.id):updateOrCreateTask()
+    renderTasks(task?.id)
+    hideTaskForm()
+    document.querySelector(".selected-hour")?.classList.remove("selected-hour");
 }
 
 function handleHourLeftClick(event) {
+    hideTaskForm()
     event.preventDefault();
-
     document.querySelector(".selected-hour")?.classList.remove("selected-hour");
     event.target.classList.add("selected-hour");
 
-    let task = event.target.dataset;
+    let clickedTask = event.target.dataset;
+    if(!clickedTask.day){
+        handleTaskLeftClick(event.target)
+        return 
+    }
     const boundingRect = event.target.getBoundingClientRect();
-    showTaskForm(task, boundingRect);
+    showTaskForm(clickedTask, boundingRect);
+}
+
+// ----------- Drag Handlers 
+
+function handleTaskDragStart(event) {
+    hideTaskForm()
+    event.dataTransfer.setData("text/plain", event.target.id);
 }
 
 function handleHourDragOver(event) {
     event.preventDefault();
-  }
+}
 
 function handleHourDrop(event) {
     const id = event.dataTransfer.getData("text");
     const draggableElement = document.getElementById(id);
     const dropzone = event.target;
+    
+
+    const taskIndex = tasks.findIndex(task=>task.id===draggableElement.id)
+    const task = tasks[taskIndex]
+
+    task["day"] = dropzone.dataset["day"] 
+    task["hour"] = dropzone.dataset["hour"]
+    
+    task["startTime"] = to24Hrs(dropzone.dataset["hour"])
+
+    task[taskIndex] = task
     dropzone.appendChild(draggableElement);
     event.dataTransfer.clearData();
+
 }
 
-function handleTaskDragStart(event) {
-    event.dataTransfer.setData("text/plain", event.target.id);
-    console.log(event.dataTransfer.getData("text"));
+
+
+// ------------ Renderer and Aux fns ------------
+const drawTask = (task)=>{
+    const taskTemplate = document.querySelector("#task-template")
+    const taskElem = taskTemplate.cloneNode(true)
+    const parentElem = document.querySelector(`[data-day="${task.day}"][data-hour="${task.hour}"]`)
+    const height = Math.max(task["duration"] * 3.65, 1)
+
+    taskElem.id = task["id"]
+    taskElem.textContent = task["title"]
+    taskElem.addEventListener("click", handleHourLeftClick)
+    taskElem.addEventListener("dragstart", handleTaskDragStart);
+    taskElem.style["height"] = height + "rem"
+    parentElem.appendChild(taskElem)
+
+    return 
 }
 
-function handleTaskLeftClick(event) {
-    const task = event.target.parentElement.dataset;
-    console.log(task)
-    const boundingRect = event.target.parentElement.getBoundingClientRect();
-    showTaskForm(task, boundingRect);
+function renderTasks(taskID){
+    // Use the task template to generate an new task element.
+    if(taskID){
+        let task = getTaskByID(taskID)
+        console.log(task)
+        document.getElementById(task.id)?.remove()
+        drawTask(task)
+    }else{
+
+        tasks.map(task=>{
+            document.getElementById(task.id)?.remove()
+            drawTask(task)
+        })
+    }
 }
 
 // ------------ Event Binders ------------
 taskForm.addEventListener("submit", handleTaskSubmit);
+document.addEventListener("keydown", handleTaskFormClose)
+taskElements.forEach((task) => {
+    task.addEventListener("dragstart", handleTaskDragStart);
+    task.addEventListener("click", handleTaskLeftClick);
+});
+
 hours.forEach((hour) => {
     hour.addEventListener("click", handleHourLeftClick);
     hour.addEventListener("dragover", handleHourDragOver);
     hour.addEventListener("drop", handleHourDrop);
 });
-tasks.forEach((task) => {
-    task.addEventListener("dragstart", handleTaskDragStart);
-    task.addEventListener("click", handleTaskLeftClick);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+// const weekDays = [
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+//     "Sunday",
+// ];
+// const dayHours = [
+//     "1:00AM",
+//     "2:00AM",
+//     "3:00AM",
+//     "4:00AM",
+//     "5:00AM",
+//     "6:00AM",
+//     "7:00AM",
+//     "8:00AM",
+//     "9:00AM",
+//     "10:00AM",
+//     "11:00AM",
+//     "12:00AM",
+//     "1:00PM",
+//     "2:00PM",
+//     "3:00PM",
+//     "4:00PM",
+//     "5:00PM",
+//     "6:00PM",
+//     "7:00PM",
+//     "8:00PM",
+//     "9:00PM",
+//     "10:00PM",
+//     "11:00PM",
+//     "12:00PM",
+// ];
+
