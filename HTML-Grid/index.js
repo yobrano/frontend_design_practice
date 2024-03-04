8736283a6a6fb18e4df030d2fbb6ee8338d3c238
yobrano <mburubrian2@gmail.com>
2024-03-04 17:43:55 +0300

    extends to next day

--- HTML-Grid/index.js
+++ HTML-Grid/index.js
@@ -12,19 +12,20 @@ const monthDays = [
     31, 30, 31, 31,
     30, 31, 30, 31
 ]
+const cellHeight = 3.65
 
 
 const tasks = []
 let taskFormIsOpen = false;
 let selectedDate = new Date()
-let isLeap = (offsetDate(selectedDate, -5).getYear() - 100 )% 4 === 0
-if(isLeap){
+let isLeap = (offsetDate(selectedDate, -5).getYear() - 100) % 4 === 0
+if (isLeap) {
     monthDays[1] = 29
 }
 
 // ------------ Helper Functions ------------
 
-function formatDate(date){
+function formatDate(date) {
 
     date = new Date(date)
     // return Intl.DateTimeFormat("en-GB").format(date)
@@ -32,33 +33,33 @@ function formatDate(date){
     let month = String(date.getMonth() + 1)
     let year = String((date.getYear() - 100) + 2000)
 
-    monthDay = monthDay.length === 1? `0${monthDay}`: monthDay
-    month = month.length === 1? `0${month}`: month
+    monthDay = monthDay.length === 1 ? `0${monthDay}` : monthDay
+    month = month.length === 1 ? `0${month}` : month
     return `${year}-${month}-${monthDay}`
 
 
 
 }
 
-function offsetDate(date, offset){
+function offsetDate(date, offset) {
     // Convert to unix time (no. of milliseconds since 01/01/1970)
-    const unixDate = date.valueOf() 
-    let hours = offset * 24 
+    const unixDate = date.valueOf()
+    let hours = offset * 24
     let minutes = hours * 60
-    let seconds = minutes * 60 
-    let milliSeconds = seconds  * 1000
+    let seconds = minutes * 60
+    let milliSeconds = seconds * 1000
 
     // offset the current date using the milliseconds then generate a new date. 
     const newUnixDate = unixDate + milliSeconds
-    
+
     return new Date(newUnixDate)
 }
 
-function getWeekRange(startDate){
+function getWeekRange(startDate) {
     let date = new Date(startDate)
     let day = date.getDay()
     let offsets = {}
-    for(let dayOffset=0; dayOffset<=6; dayOffset++){
+    for (let dayOffset = 0; dayOffset <= 6; dayOffset++) {
         let wkDay = offsetDate(date, dayOffset - day)
         let index = weekDays[wkDay.getDay()]
         offsets[index] = wkDay
@@ -85,43 +86,42 @@ function timeDiff(t2, t1) {
 
     minDiff = ((time2 - time1) / 1000) / 60
     hrsDiff = minDiff / 60
-    console.log(hrsDiff)
     return hrsDiff
 
 }
 
-function to24Hrs(time){
-    let AM_PM = time.slice(-2) 
+function to24Hrs(time) {
+    let AM_PM = time.slice(-2)
     let [hours, minutes] = time.slice(0, -2).split(":")
-    if(hours < 12 && AM_PM === "PM"){
+    if (hours < 12 && AM_PM === "PM") {
         hours = Number(hours) + 12
-    }else if(hours === "12" && AM_PM === "AM"){
+    } else if (hours === "12" && AM_PM === "AM") {
         hours = "00"
     }
 
     hours = String(hours)
     minutes = String(minutes)
 
-    hours = hours.length === 1? `0${hours}`: hours
-    minutes = minutes.length === 1? `0${minutes}`: minutes
+    hours = hours.length === 1 ? `0${hours}` : hours
+    minutes = minutes.length === 1 ? `0${minutes}` : minutes
 
     return `${hours}:${minutes}`
 }
 
-function to12Hrs(time){
-    if(!time) return ""
+function to12Hrs(time) {
+    if (!time) return ""
 
     let [hours, minutes] = time.split(":")
     hours = Number(hours)
     minutes = Number(minutes)
 
-    if(hours < 12){
-        hours = hours === 0? "12" : hours
+    if (hours < 12) {
+        hours = hours === 0 ? "12" : hours
         return `${hours}:${minutes}AM`
     }
     else if (hours === 12) {
         return `${hours}:${minutes}PM`
-    } 
+    }
     else {
         return `${hours - 12}:${minutes}PM`
     }
@@ -129,23 +129,23 @@ function to12Hrs(time){
 
 // Task CRUD functions
 
-function getTaskByHour({day, hour}){
-    
-    const task = tasks.filter(task=>{
-        return task.hour === hour && task.day === day 
+function getTaskByHour({ day, hour }) {
+
+    const task = tasks.filter(task => {
+        return task.hour === hour && task.day === day
     })[0]
     return task
 }
 
 function getTaskByID(taskID) {
     console.log(taskID)
-    return tasks.filter (task => task.id === taskID)[0]
+    return tasks.filter(task => task.id === taskID)[0]
 }
 
 function updateOrCreateTask(taskID) {
     // Read fomr data.
     const id = taskForm.querySelector("input[name=task_id]").value
-    const day = taskForm.querySelector("input[name=task_day]").value
+    let day = taskForm.querySelector("input[name=task_day]").value
     let hour = taskForm.querySelector("input[name=task_hour]").value
     const date = taskForm.querySelector("input[name=task_date]").value
     const title = taskForm.querySelector("input[name=task_title]").value
@@ -153,24 +153,29 @@ function updateOrCreateTask(taskID) {
     const completed = taskForm.querySelector("input[name=task_completed]").checked
     const startTime = taskForm.querySelector("input[name=task_start_time]").value
     const description = taskForm.querySelector("textarea[name=task_description]").value
-    
+
     // Calculated Fields.
-    const duration = (endTime!=="" && startTime!=="")? timeDiff(endTime, startTime) : ""
+    const duration = (endTime !== "" && startTime !== "") ? timeDiff(endTime, startTime) : ""
     if (startTime) {
         let time = to12Hrs(startTime)
         let startHour = time.split(":")[0]
         let AM_PM = time.slice(-2)
         hour = `${startHour}:00${AM_PM}`
     }
-    
-    const taskItem = {id, day, hour, completed, date, startTime, endTime, duration, title, description }
+
+    if (date) {
+        let temp = new Date(date)
+        day = weekDays[temp.getDay()]
+    }
+
+    const taskItem = { id, day, hour, completed, date, startTime, endTime, duration, title, description }
 
     // Create if id is null or ""
-    if(id){
+    if (id) {
         // update Task
         const taskIndex = tasks.indexOf(getTaskByID(taskID))
         tasks[taskIndex] = taskItem
-    }else{
+    } else {
         // Create Task
         taskItem.id = crypto.randomUUID()
         tasks.push(taskItem)
@@ -178,8 +183,8 @@ function updateOrCreateTask(taskID) {
     return taskItem
 }
 
-function deleteTask(taskID){
-    const taskIndex = tasks.findIndex(task=> task.id === taskID)
+function deleteTask(taskID) {
+    const taskIndex = tasks.findIndex(task => task.id === taskID)
     tasks.splice(taskIndex, 1)
 }
 
@@ -212,16 +217,16 @@ function hideTaskForm() {
 function showTaskForm(clickedTask, pos) {
 
     const task = getTaskByHour(clickedTask)
-    if(!task){
-        taskForm.querySelector("input[name=task_day]").value = clickedTask["day"] ;
-        taskForm.querySelector("input[name=task_hour]").value = clickedTask["hour"] ;
+    if (!task) {
+        taskForm.querySelector("input[name=task_day]").value = clickedTask["day"];
+        taskForm.querySelector("input[name=task_hour]").value = clickedTask["hour"];
         taskForm.querySelector("input[name=task_start_time]").value = to24Hrs(clickedTask["hour"]);
         taskForm.querySelector("input[name=task_date]").value = formatDate(clickedTask["date"]);
-    }else{
-        taskForm.querySelector("input[name=task_id]").value = task["id"] ;
-        taskForm.querySelector("input[name=task_date]").value = task["date"] ;
+    } else {
+        taskForm.querySelector("input[name=task_id]").value = task["id"];
+        taskForm.querySelector("input[name=task_date]").value = task["date"];
         taskForm.querySelector("input[name=task_title]").value = task["title"];
-        taskForm.querySelector("input[name=task_day]").value = clickedTask["day"] ;
+        taskForm.querySelector("input[name=task_day]").value = clickedTask["day"];
         taskForm.querySelector("input[name=task_hour]").value = clickedTask["hour"];
         taskForm.querySelector("input[name=task_end_time]").value = task["endTime"];
         taskForm.querySelector("input[name=task_completed]").value = task["completed"];
@@ -235,7 +240,7 @@ function showTaskForm(clickedTask, pos) {
     taskForm.style["left"] = pos.x + "px";
     taskForm.style["top"] = y + "px";
     taskForm.style["display"] = "block";
-    
+
     taskFormIsOpen = true;
 }
 
@@ -245,41 +250,39 @@ function showTaskForm(clickedTask, pos) {
 function handleTaskFormClose(event) {
     if (event.key === "Escape" && taskFormIsOpen) {
         hideTaskForm()
-}
+    }
 }
 
 // ----------- Click Handlers
 function handleTaskLeftClick(element) {
     hideTaskForm()
     element.parentElement.classList.add("selected-hour");
-    
+
     const clickedTask = element.parentElement.dataset;
     const boundingRect = element.parentElement.getBoundingClientRect();
     showTaskForm(clickedTask, boundingRect);
 }
 
-function handleTaskSubmit(event){
+function handleTaskSubmit(event) {
 
     event.preventDefault()
     const id = taskForm.querySelector("input[name=task_id]").value
     const task = getTaskByID(id)
     //create if task is empty 
-    task? updateOrCreateTask(task.id):updateOrCreateTask()
+    task ? updateOrCreateTask(task.id) : updateOrCreateTask()
     renderTasks(task?.id)
     hideTaskForm()
     document.querySelector(".selected-hour")?.classList.remove("selected-hour");
 }
 
-
-
-function handleDeleteTask(event){
+function handleDeleteTask(event) {
     event.preventDefault()
     const id = taskForm.querySelector("input[name=task_id]").value
     hideTaskForm()
     const taskElement = document.getElementById(id)
     taskElement.remove()
     deleteTask(id)
-    
+
 }
 
 
@@ -290,9 +293,9 @@ function handleHourLeftClick(event) {
     event.target.classList.add("selected-hour");
 
     let clickedTask = event.target.dataset;
-    if(!clickedTask.day){
+    if (!clickedTask.day) {
         handleTaskLeftClick(event.target)
-        return 
+        return
     }
     const boundingRect = event.target.getBoundingClientRect();
     showTaskForm(clickedTask, boundingRect);
@@ -303,6 +306,9 @@ function handleHourLeftClick(event) {
 function handleTaskDragStart(event) {
     hideTaskForm()
     event.dataTransfer.setData("text/plain", event.target.id);
+
+    const task = getTaskByID(event.target.id)
+    document.querySelector(`[id="${task.id}"][data-extension=true]`)?.remove()    
 }
 
 function handleHourDragOver(event) {
@@ -313,18 +319,19 @@ function handleHourDrop(event) {
     const id = event.dataTransfer.getData("text");
     const draggableElement = document.getElementById(id);
     const dropzone = event.target;
-    
 
-    const taskIndex = tasks.findIndex(task=>task.id===draggableElement.id)
+
+    const taskIndex = tasks.findIndex(task => task.id === draggableElement.id)
     const task = tasks[taskIndex]
 
-    task["day"] = dropzone.dataset["day"] 
+    task["day"] = dropzone.dataset["day"]
     task["hour"] = dropzone.dataset["hour"]
-    task["date"]= dropzone.dataset["date"]
+    task["date"] = dropzone.dataset["date"]
 
     task["startTime"] = to24Hrs(dropzone.dataset["hour"])
 
-    task[taskIndex] = task
+    tasks[taskIndex] = task
+    drawNextDay(task,)
     dropzone.appendChild(draggableElement);
     event.dataTransfer.clearData();
 
@@ -333,37 +340,65 @@ function handleHourDrop(event) {
 
 
 // ------------ Renderer and Aux fns ------------
-const drawTask = (task)=>{
+const drawTask = (task) => {
     const taskTemplate = document.querySelector("#task-template")
     const taskElem = taskTemplate.cloneNode(true)
+
     const parentElem = document.querySelector(`[data-day="${task.day}"][data-hour="${task.hour}"]`)
-    const height = Math.max(task["duration"] * 3.65, 1)
     
     taskElem.id = task["id"]
     taskElem.textContent = task["title"]
     taskElem.addEventListener("click", handleHourLeftClick)
     taskElem.addEventListener("dragstart", handleTaskDragStart);
-    console.log(task)
+    
+    let height = Math.max(task["duration"] * cellHeight, 1)
+    if(task.isExtension){
+        taskElem.dataset["extension"] = true;
+        height = Math.max(task["duration"] * cellHeight, 1)
+    }
+
     taskElem.style["height"] = height + "rem"
     parentElem.appendChild(taskElem)
-
-    return 
 }
 
-function renderTasks(taskID){
+function renderTasks(taskID) {
     // Use the task template to generate an new task element.
-    if(taskID){
+    if (taskID) {
         let task = getTaskByID(taskID)
         document.getElementById(task.id)?.remove()
         drawTask(task)
-    }else{
+    } else {
 
-        tasks.map(task=>{
+        tasks.map(task => {
             // Remove existing element
             document.getElementById(task.id)?.remove()
             drawTask(task)
         })
     }
+}
+
+
+function drawNextDay(task, startHour) {
+    startHour = startHour ? startHour : "12:00AM"
+    const hoursToEOD = timeDiff("23:59", task.startTime) // Hours to end of day
+    const excess = hoursToEOD - task.duration
+
+    if (excess < 0) {
+        let nextDay = offsetDate(new Date(task.date), 1)
+
+        const extendedTask = {
+            ...task,
+            duration: Math.abs(excess),
+            day: weekDays[nextDay.getDay()],
+            hour: startHour,
+            title: task.title + " cnt'd",
+            isExtension: true
+        }
+        document.getElementById(task.id).height = String(hoursToEOD * cellHeight)+ "rem!important"
+        console.log(document.getElementById(task.id))
+        drawTask(extendedTask)
+    }
+
 }
 
 // ------------ Event Binders ------------
